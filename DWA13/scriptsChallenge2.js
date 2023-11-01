f51c292dc9d86e880e22a8771990f79dadda11cd
Shabbeer Petersen <shabbeerexilepetersen@gmail.com>
2023-11-01 11:31:54 +0200

    updated node_modules

--- DWA13/scriptsChallenge2.js
+++ DWA13/scriptsChallenge2.js
@@ -2,7 +2,7 @@ const products = [
   { product: "banana", price: "2" },
   { product: "mango", price: 6 },
   { product: "potato", price: " " },
-  { product: "avocado", price: "8" },
+  { product: "coffee", price: "8" },
   { product: "coffee", price: 10 },
   { product: "tea", price: "" },
 ];
